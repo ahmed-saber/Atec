8889326b2665c5938e4ce3840e3f95036ff9a447
Ahmed Saber <smartic@gmail.com>
2018-01-12 22:28:39 +0200

    Adding style for single pages

--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -1,18 +1,34 @@
+<?php /* Template Name: About */ ?>
 <?php get_header(); ?>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
+<?php
+if (has_post_thumbnail($post->ID)){
+    $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+}
+?>
+<!--first section-->
+<section class="block-st1 firstSec" style="background-image:url('<?php echo ($image[0]) ? $image[0] : get_template_directory_uri(); ?>/assets/img/bg/BG6.jpg; '">
+    <div class="logo_container anim1 hidden">
+        <span class="logo"></span>
+        <span class="no-selection BG_text"><?php _e('atec'); ?></span>
+    </div>
+    <?php
+    include('aside.php');
+    ?>
+    <div class="title-st1">
+        <h1><?php the_field('title'); ?></h1>
+        <h2><?php the_field('sub_title'); ?></h2>
+    </div>
 </section>
-</article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-<?php endwhile; endif; ?>
+<!--second section-->
+<section id="second_sec" class="sec second_sec">
+    <div class="max_w">
+        <div class="flex">
+            <div class="col col--12 col__md--12 col__lg--12">
+                <div class="content-block">
+                    <?php echo wpautop($post->post_content); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
