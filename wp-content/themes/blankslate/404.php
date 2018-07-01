204380b01e88a676e854d1eecfc2e493b8609b8d
Ahmed Saber <smartic@gmail.com>
2018-07-01 14:04:03 +0200

    adding 404 page

--- wp-content/themes/blankslate/404.php
+++ wp-content/themes/blankslate/404.php
@@ -1,14 +1,9 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<section class="entry-content">
-<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
-</section>
-</article>
-</section>
-<?php get_sidebar(); ?>
+<?php
+$contentHeaderData = "
+    <h1>".__( 'Not Found', 'blankslate' )."</h1>
+    <h2>".__( 'Nothing found for the requested page. Try a search instead?', 'blankslate' )."</h2>
+";
+include(locate_template('content-header.php',false,false));
+?>
 <?php get_footer(); ?>
